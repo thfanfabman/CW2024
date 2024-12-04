80ef55b5446b62ea32d4dd79880e9fb9ae51e1a2
Fan Zheng <thfanfabman@gmail.com>
2024-12-04 23:11:09 +0800

    changed main menu visual

--- src/main/java/com/example/demo/controller/MainMenu.java
+++ src/main/java/com/example/demo/controller/MainMenu.java
@@ -4,6 +4,9 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -25,12 +28,17 @@ public class MainMenu {
     public void showMenu() {
         // Create a label for the main menu text
         Label menuLabel = new Label("Welcome to Sky Battle");
-        menuLabel.setFont(new Font("Arial", 24));
+        menuLabel.setFont(new Font("Arial", 50));
         menuLabel.setTextFill(Color.BLACK);
 
         // Create buttons for "Start Game" and "Exit"
         Button startButton = new Button("Start Game");
+        startButton.setFont(new Font("Arial", 16));
+        startButton.setPrefWidth(150);
         Button exitButton = new Button("Exit");
+
+        exitButton.setFont(new Font("Arial", 16));
+        exitButton.setPrefWidth(150);
 
         // Set button actions
         startButton.setOnAction(event -> {
@@ -43,10 +51,11 @@ public class MainMenu {
         });
         exitButton.setOnAction(event -> stage.close());
 
-        // Use VBox to align the label and buttons vertically
-        VBox menuBox = new VBox(10); // The number 10 specifies spacing between elements
+        VBox menuBox = new VBox(10);
         menuBox.setAlignment(Pos.CENTER);
         menuBox.getChildren().addAll(menuLabel, startButton, exitButton);
+        menuBox.setBackground(new Background(new BackgroundFill(Color.SKYBLUE, CornerRadii.EMPTY, null)));
+
 
         // Set up the scene and display it in the stage
         Scene scene = new Scene(menuBox, 400, 300);
